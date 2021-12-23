6803e4a95a78386e1e317077e605e102621e6ba6
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-23 11:43:47 -0300

    initial commit

---
+++ src/test/java/xyz/marcelosantos/vendasapi/VendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.marcelosantos.vendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
